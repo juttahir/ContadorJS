86d341785850b07e81687e9b5eea9a60bcc7f7f2
Juttahir <juttahir@hotmail.com>
2022-02-01 15:58:19 -0300

    First commit

---
+++ assets/scripts.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+function increment(){
+    currentNumber = currentNumber +1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    currentNumber = currentNumber -1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function play() {
+    document.getElementById("Player").play();
+  }
+  
+  play();
